17d3260281b3ede4991c2a04353cb59c78c1f4cd
t0mm4rx <tom.marx@hotmail.fr>
2019-09-23 16:27:44 +0200

    Disqus integration + correction min-width sur mobile

--- blog_article.php
+++ blog_article.php
@@ -10,4 +10,19 @@ $post = $blog->get_post_by_link($article);
 
 <section id="article-body">
     <?php include_once("ressources/articles/" . $article . ".php"); ?>
+    <div id="disqus_thread"></div>
+    <script>
+    var disqus_config = function () {
+    this.page.url = '<?php echo $post->get_url(); ?>';
+    this.page.identifier = '<?php echo $post->get_link(); ?>';
+    };
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://tom-marx.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
+    </script>
+    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+    </div>
 </section>
