c5982c86fd691b9241af24f68a4952800b416ec2
Tom <tom.marx@hotmail.fr>
2021-01-02 10:49:16 +0100

    Kaggle

--- me.php
+++ me.php
@@ -2,6 +2,7 @@
 <div id="socials-header">
   <div>
       <a href="https://github.com/t0mm4rx" target="_blank" class="button-icon"><i class="fab fa-github"></i></a>
+      <a href="https://www.kaggle.com/tommarx" target="_blank" class="button-icon"><i class="fab fa-kaggle"></i></a>
       <a href="https://twitter.com/AirM4rx" target="_blank" class="button-icon"><i class="fab fa-twitter"></i></a>
       <a href="https://codepen.io/t0mm4rx/" target="_blank" class="button-icon"><i class="fab fa-codepen"></i></a>
       <a href="https://www.malt.fr/profile/tommarx" target="_blank" class="button-icon"><image src="<?php echo $GLOBALS['url']; ?>ressources/malt.svg"
